cbe5c6659b1e9c5e1f92f98ae6096c1be1372328
root <root@a89faa1e4228>
2020-11-12 03:25:10 +0000

    RaceCar

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -11,9 +11,9 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	
+	RaceCar car = new RaceCar("Mclaren", 2);
 		// 2. Print the RaceCar's position in the race
-
+	System.out.println(car.getPositionInRace());
 		// 3. Crash the RaceCar
 		
 		// 4. If the car is damaged. Bring it in for a pit stop.
