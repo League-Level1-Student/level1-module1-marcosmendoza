de3f010f394303c724a6d78e26e7107c093b512a
root <root@a6f69a570524>
2020-12-10 03:26:48 +0000

    PopcornMaker

--- src/_04_popcorn/PopcornMaker.java
+++ src/_04_popcorn/PopcornMaker.java
@@ -1,5 +1,37 @@
 package _04_popcorn;
 
 public class PopcornMaker {
+    public static void main(String args[])  {
+    	
+    	
+    	Popcorn();
+    	
+    	Microwave(); 
+    	
+    	thingToBeCooked("Popcorn");
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+    	
+}
 
+	private static void Popcorn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void thingToBeCooked(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void Microwave() {
+		// TODO Auto-generated method stub
+		
+	}
 }
