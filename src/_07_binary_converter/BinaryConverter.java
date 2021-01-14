3ab78d5686e5a6b8e5245b5edd55548e6ef1d03d
root <root@ee4b147f6f91>
2021-01-14 03:30:51 +0000

    BinaryConverter

--- src/_07_binary_converter/BinaryConverter.java
+++ src/_07_binary_converter/BinaryConverter.java
@@ -1,14 +1,34 @@
 package _07_binary_converter;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class BinaryConverter {
+public class BinaryConverter implements ActionListener {
 public static void main(String[] args) {
+	
+
 	
 	JFrame frame = new JFrame();
 	JPanel panel = new JPanel();
 	JTextField textfield = new JTextField(20);
+}
+
+public void button(String[] args) {
+
+	 JButton button = new JButton();
+	 button.addActionListener(this);
+	
+	
+}
+
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	// TODO Auto-generated method stub
+	
 }
 }
