6a6e87cd2a99c1ef09ace457630f0d2c072585e8
student <student@ip-172-31-62-180.us-west-2.compute.internal>
2021-01-27 19:29:49 -0800

    BinaryConverter

--- src/_07_binary_converter/BinaryConverter.java
+++ src/_07_binary_converter/BinaryConverter.java
@@ -5,30 +5,52 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class BinaryConverter implements ActionListener {
-public static void main(String[] args) {
-	
+	String convert(String input) {
+		if (input.length() != 8) {
+			JOptionPane.showMessageDialog(null, "Enter 8 bits, silly!!!");
+			return "-";
+		}
+		String binary = "[0-1]+";// must contain numbers in the given range
+		if (!input.matches(binary)) {
+			JOptionPane.showMessageDialog(null, "Binary can only contain 1s or 0s, silly!!!");
+			return "-";
+		}
+		try {
+			int asciiValue = Integer.parseInt(input, 2);
+			char theLetter = (char) asciiValue;
+			return "" + theLetter;
+		} catch (Exception e) {
+			JOptionPane.showMessageDialog(null, "Enter a binary, silly!!!");
+			return "-";
+		}
+	}
 
-	
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	JTextField textfield = new JTextField(20);
-}
+	public static void main(String[] args) {
 
-public void button(String[] args) {
+		
 
-	 JButton button = new JButton();
-	 button.addActionListener(this);
-	
-	
-}
+	}
 
-@Override
-public void actionPerformed(ActionEvent arg0) {
-	// TODO Auto-generated method stub
-	
-}
+	public void button(String[] args) {
+
+		JButton button = new JButton();
+		button.addActionListener(this);
+		JFrame frame = new JFrame();
+		JPanel panel = new JPanel();
+		JTextField textfield = new JTextField(20);
+		JLabel label = new JLabel();
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+
+	}
 }
